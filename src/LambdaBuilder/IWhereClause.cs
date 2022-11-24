a790a983fdbf9583852a6327b825bb5cf037898d
Yılmaz Buhar <yilmaz.buhar@eplatform.com.tr>
2022-11-24 16:26:49 +0300

    feat(sorting) create sort lambda

--- src/LambdaBuilder/IWhereClause.cs
+++ src/LambdaBuilder/IWhereClause.cs
@@ -33,34 +33,34 @@ namespace LambdaBuilder
         }
     }
 
-    public class WhereClause
+    public class WhereClause : IWhereClause
     {
-        //    public static IQueryable<T> Where<T>(this IQueryable<T> query, string selector, string comparer, string value)
-        //    {
-        //        var target = Expression.Parameter(typeof(T));
+        public static IQueryable<T> Where<T>(this IQueryable<T> query, string selector, string comparer, string value)
+        {
+            var target = Expression.Parameter(typeof(T));
 
-        //        return query.Provider.CreateQuery<T>(CreateWhereClause(target, query.Expression, selector, comparer, value));
-        //    }
+            return query.Provider.CreateQuery<T>(CreateWhereClause(target, query.Expression, selector, comparer, value));
+        }
 
-        //    static Expression CreateWhereClause(ParameterExpression target, Expression expression, string selector, string comparer, string value)
-        //    {
-        //        var predicate = Expression.Lambda(CreateComparison(target, selector, comparer, value), target);
+        static Expression CreateWhereClause(ParameterExpression target, Expression expression, string selector, string comparer, string value)
+        {
+            var predicate = Expression.Lambda(CreateComparison(target, selector, comparer, value), target);
 
-        //        return Expression.Call(typeof(Queryable), nameof(Queryable.Where), new[] { target.Type },
-        //            expression, Expression.Quote(predicate));
-        //    }
+            return Expression.Call(typeof(Queryable), nameof(Queryable.Where), new[] { target.Type },
+                expression, Expression.Quote(predicate));
+        }
 
-        //    static Expression CreateComparison(ParameterExpression target, string selector, string comparer, string value)
-        //    {
-        //        var memberAccess = CreateMemberAccess(target, selector);
-        //        var actualValue = Expression.Constant(value, typeof(string));
+        static Expression CreateComparison(ParameterExpression target, string selector, string comparer, string value)
+        {
+            var memberAccess = CreateMemberAccess(target, selector);
+            var actualValue = Expression.Constant(value, typeof(string));
 
-        //        return Expression.Call(memberAccess, comparer, null, actualValue);
-        //    }
+            return Expression.Call(memberAccess, comparer, null, actualValue);
+        }
 
-        //    static Expression CreateMemberAccess(Expression target, string selector)
-        //    {
-        //        return selector.Split('.').Aggregate(target, (t, n) => Expression.PropertyOrField(t, n));
-        //    }
+        static Expression CreateMemberAccess(Expression target, string selector)
+        {
+            return selector.Split('.').Aggregate(target, (t, n) => Expression.PropertyOrField(t, n));
+        }
     }
 }
