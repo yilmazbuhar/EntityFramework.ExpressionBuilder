6eee29402409d93f4948f6fb69e57ba634664ca0
YILMAZCAN\yilmaz <yilmazbuhar@gmail.com>
2022-12-02 00:13:34 +0300

    feat(test) northwind test project

---
+++ src/LambdaBuilder.Northwind/Model/Region.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LambdaBuilder.Northwind;
+
+public partial class Region
+{
+    public int RegionId { get; set; }
+
+    public string RegionDescription { get; set; } = null!;
+
+    public virtual ICollection<Territory> Territories { get; } = new List<Territory>();
+}
---
+++ src/LambdaBuilder.Northwind/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LambdaBuilder;
+using LambdaBuilder.Northwind;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+var hostBuilder = Host.CreateDefaultBuilder()
+    .ConfigureAppConfiguration(cb => {
+        cb.AddJsonFile("appsettings.json", false);
+    })
+    .ConfigureServices((ctx,services) => {
+        services.AddDbContext<NorthwindContext>(optionBuilder => {
+            optionBuilder.UseSqlServer(ctx.Configuration.GetConnectionString("default"));
+        });
+    })
+    .ConfigureLogging(x => {
+        x.ClearProviders();
+    })
+    .Build();
+
+var dbContext = hostBuilder.Services.GetService<NorthwindContext>();
+
+
+// Generate mock request --------------------------------------------------
+var jsonFilter = File.ReadAllText("filterdata.json");
+
+// Send to database -------------------------------------------------------
+var data = await dbContext.Orders.ApplyFilterAndSort(jsonFilter, null);
+
+foreach (var item in data)
+{
+    Console.WriteLine($"{item.ShipName} -> {item.OrderDate}");
+}
